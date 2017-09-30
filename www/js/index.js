0eafe24d3a78d918d4ba448ff2e2c84ac158b358
ymatsuo <cigalecigales@gmail.com>
2017-09-30 21:04:48 +0900

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,59 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    onDeviceReady: function() {
+        this.receivedEvent('install');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var element = document.getElementById(id);
+
+        element.addEventListener('click', function() {
+
+            var fileTransfer = new FileTransfer();
+
+            // Get cordova file data directory (app sandbox directory)
+            //  > file:///data/user/0/io.cordova.apk.installer.sample/files/
+            var sandBoxDirectory = cordova.file.dataDirectory;
+
+            // Please set apk download path
+            var apkUrl = 'http://example.com/sample.apk';
+
+            // Get file name by apk url;
+            var fileName = apkUrl.match(/[^/]+$/i)[0];
+
+            var modal = document.querySelector('ons-modal');
+
+            modal.show();
+
+            fileTransfer.download(
+                apkUrl,
+                sandBoxDirectory + fileName,
+                function(entry) {
+                    console.log("download success: " + entry.toURL());
+                    // Install app
+                    apkInstaller.install(fileName, function(msg) {
+                        // Start the installer
+                        modal.hide();
+                    }, function(error) {
+                        modal.hide();
+                        ons.notification.alert('Install Error !!: ' + error);
+                    });
+                },
+                function(error) {
+                    modal.hide();
+                    ons.notification.alert('Download Error !!: ' + error);
+                },
+                false, {}
+            );
+
+        }, false);
+    }
+};
+
+app.initialize();
